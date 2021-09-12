2fba4c090f57c0432402a0433b0c26b19bf8b317
apoorvsingh2000 <apoorvsinghlko@gmail.com>
2021-09-12 17:46:06 +0530

    🎟basic todo list done,doing timestamp items

--- lib/models/VideoPlayer.dart
+++ lib/models/VideoPlayer.dart
@@ -1,6 +1,5 @@
 import 'package:cached_video_player/cached_video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 class VideoPlayerCustom extends StatefulWidget {
   final _videoUrl;
